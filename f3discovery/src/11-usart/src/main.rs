d39f2aa3f95bf64fc3bf7a232b3d7c8da5972d85
Lukas Grabowski <gregorylakewood@gmail.com>
2022-10-29 18:42:40 +0200

    11 - usart

--- f3discovery/src/11-usart/src/main.rs
+++ f3discovery/src/11-usart/src/main.rs
@@ -2,16 +2,49 @@
 #![no_std]
 
 #[allow(unused_imports)]
-use aux11::{entry, iprint, iprintln};
+use aux11::{entry, iprint, iprintln, usart1::RegisterBlock};
+
+const TEXT: &str = "The quick brown fox jumps over the lazy dog.";
 
 #[entry]
 fn main() -> ! {
     let (usart1, _mono_timer, _itm) = aux11::init();
 
+    // Send a string
+    for letter in TEXT.encode_utf16() {
+        usart1.tdr.write(|w| w.tdr().bits(letter));
+    }
+
     // Send a single character
     usart1
         .tdr
         .write(|w| w.tdr().bits(u16::from(b'X')) );
+
+    loop {}
+}
+
+//#[entry]
+fn main2() -> ! {
+    let (usart1, mono_timer, mut itm) = aux11::init();
+
+    let instant = mono_timer.now();
+    // Send a string
+    for byte in b"The quick brown fox jumps over the lazy dog.".iter() {
+        // wait until it's safe to write to TDR
+        while usart1.isr.read().txe().bit_is_clear() {} // <- NEW!
+
+        usart1
+            .tdr
+            .write(|w| w.tdr().bits(u16::from(*byte)));
+    }
+    let elapsed = instant.elapsed(); // in ticks
+
+    iprintln!(
+        &mut itm.stim[0],
+        "`for` loop took {} ticks ({} us)",
+        elapsed,
+        elapsed as f32 / mono_timer.frequency().0 as f32 * 1e6
+    );
 
     loop {}
 }
