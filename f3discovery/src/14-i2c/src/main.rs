4dc64e2d6d1ec5686859f76212a615e074c10c53
Grabowski Łukasz <p2042@PCOSA.AD>
2022-11-22 15:33:15 +0100

    continue read

--- f3discovery/src/14-i2c/src/main.rs
+++ f3discovery/src/14-i2c/src/main.rs
@@ -4,16 +4,60 @@
 
 #[allow(unused_imports)]
 use aux14::{entry, iprint, iprintln, prelude::*};
-
 // Slave address
-const MAGNETOMETER: u16 = 0b0011_1100;
+const MAGNETOMETER: u16 = 0b00111100;
 
 // Addresses of the magnetometer's registers
+#[allow(dead_code)]
 const OUT_X_H_M: u8 = 0x03;
+#[allow(dead_code)]
 const IRA_REG_M: u8 = 0x0A;
 
 // LSM303AGR
 const WHO_AM_I_M: u8 = 0x4F;
+const CFG_REG_A_M: u8 = 0x60;
+
+fn set_continous(i2c1: &aux14::i2c1::RegisterBlock) {
+    {
+        i2c1.cr2.write(|w| {
+            w.start().set_bit();
+            w.sadd().bits(MAGNETOMETER);
+            w.rd_wrn().clear_bit();
+            w.nbytes().bits(1);
+            w.autoend().clear_bit()
+        });
+        while i2c1.isr.read().txis().bit_is_clear() {}
+        i2c1.txdr.write(|w| w.txdata().bits(CFG_REG_A_M));
+        while i2c1.isr.read().tc().bit_is_clear() {}
+    }
+
+    let byte = {
+        i2c1.cr2.modify(|_, w| {
+            w.start().set_bit();
+            w.nbytes().bits(1);
+            w.rd_wrn().set_bit();
+            w.autoend().set_bit()
+        });
+        while i2c1.isr.read().rxne().bit_is_clear() {}
+        i2c1.rxdr.read().rxdata().bits()
+    };
+
+    let new_config = byte & 0b1111_1100;
+
+    {
+        i2c1.cr2.write(|w| {
+            w.start().set_bit();
+            w.sadd().bits(MAGNETOMETER);
+            w.rd_wrn().clear_bit();
+            w.nbytes().bits(2);
+            w.autoend().set_bit()
+        });
+        while i2c1.isr.read().txis().bit_is_clear() {}
+        i2c1.txdr.write(|w| w.txdata().bits(CFG_REG_A_M));
+        while i2c1.isr.read().txis().bit_is_clear() {}
+        i2c1.txdr.write(|w| w.txdata().bits(new_config));
+    }
+}
 
 #[entry]
 fn main() -> ! {
@@ -63,6 +107,60 @@ fn main() -> ! {
 
     // Expected output: 0x0A - 0b01001000
     iprintln!(&mut itm.stim[0], "0x{:02X} - 0b{:08b}", IRA_REG_M, byte);
+
+    {
+        i2c1.cr2.write(|w| {
+            w.start().set_bit();
+            w.sadd().bits(MAGNETOMETER);
+            w.rd_wrn().clear_bit();
+            w.nbytes().bits(1);
+            w.autoend().clear_bit()
+        });
+        while i2c1.isr.read().txis().bit_is_clear() {}
+        i2c1.txdr.write(|w| w.txdata().bits(CFG_REG_A_M));
+        while i2c1.isr.read().tc().bit_is_clear() {}
+    }
+
+    let read_config = {
+        i2c1.cr2.modify(|_, w| {
+            w.start().set_bit();
+            w.nbytes().bits(1);
+            w.rd_wrn().set_bit();
+            w.autoend().set_bit()
+        });
+        while i2c1.isr.read().rxne().bit_is_clear() {}
+        i2c1.rxdr.read().rxdata().bits()
+    };
+
+    iprintln!(&mut itm.stim[0], "0x{:02X} - 0b{:08b}", CFG_REG_A_M, read_config);
+
+    set_continous(i2c1);
+
+    {
+        i2c1.cr2.write(|w| {
+            w.start().set_bit();
+            w.sadd().bits(MAGNETOMETER);
+            w.rd_wrn().clear_bit();
+            w.nbytes().bits(1);
+            w.autoend().clear_bit()
+        });
+        while i2c1.isr.read().txis().bit_is_clear() {}
+        i2c1.txdr.write(|w| w.txdata().bits(CFG_REG_A_M));
+        while i2c1.isr.read().tc().bit_is_clear() {}
+    }
+
+    let read_config = {
+        i2c1.cr2.modify(|_, w| {
+            w.start().set_bit();
+            w.nbytes().bits(1);
+            w.rd_wrn().set_bit();
+            w.autoend().set_bit()
+        });
+        while i2c1.isr.read().rxne().bit_is_clear() {}
+        i2c1.rxdr.read().rxdata().bits()
+    };
+
+    iprintln!(&mut itm.stim[0], "0x{:02X} - 0b{:08b}", CFG_REG_A_M, read_config);
 
     loop {}
 }
